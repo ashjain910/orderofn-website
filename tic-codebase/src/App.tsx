a0be598aa062abf46401bcf388d1b14b1dada9cc
rajeshkanna <rajeshkannathiru10@gmail.com>
2025-11-27 12:35:15 +0530

    tic base file changes

--- tic-codebase/src/App.tsx
+++ tic-codebase/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/tic">
       <ScrollToTop />
       <Routes>
         {/* 👇 DEFAULT ROUTE (Login page will load on app start) */}
